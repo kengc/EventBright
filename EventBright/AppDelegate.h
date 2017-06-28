88e82778dfcc2f72856fc4872f66437396b2c993
kevin Cleathero <kengc@hotmail.com>
2017-06-28 11:13:47 -0700

    project setup

---
+++ EventBright/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventBright
+//
+//  Created by Kevin Cleathero on 2017-06-28.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
